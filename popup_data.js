ea02ec7d499c2818b7e54e2b596414aae6ba308d
muskanchohla <muskanpreet0702@gmail.com>
2025-02-15 12:17:05 -0500

    added new experience detail and 3d animation project

--- popup_data.js
+++ popup_data.js
@@ -1,53 +1,61 @@
 const popup_projects=[
-    {
-        id: 1,
-        title: "Gurbani Application",
-        description: ["Historical Information: Provides insights into Sikhism's rich history, culture, and traditions",
-"Martyrs' Profiles: Honors the sacrifices of Sikh martyrs with dedicated profiles and tributes",
-"Japji Sahib Analysis: Offers a detailed study and interpretation of Japji Sahib, enhancing users' understanding of Sikh scripture",
-"User-Friendly Interface: Designed with intuitive navigation and user interface for ease of use and accessibility."],
-        technology: "The Gurbani Application was built in Xcode using Swift, with UIKit for UI, Core Data for offline data, and AVFoundation for Gurbani audio playback. It supports responsive design via Auto Layout and uses JSON for data handling, with GitHub for version control.",
-        imgvid: "gurugranthsahib.jpg"
-      },
-      {
-        id: 2,
-        title: "Children's Game Website",
-        description: ["Created user-friendly registration and authentication system",
-"Implemented real-time validation using AJAX for enhanced user experience",
-"Designed and managed multi-level question-and-answer game structure using php, css and html",
-"Integrated secure password management functionality",
-"Developed comprehensive game results tracking and display features",
-"Utilized MySQL database for efficient data management",
-"Customized user interface for enhanced interactivity and engagement"],
-            technology: "PHP, CSS, and HTML were employed to build the game structure and interface, while AJAX provided real-time validation for an enhanced user experience. MySQL was used for efficient data management, and secure password management ensured user authentication.",
-            imgvid: "kidsgamevideo.mov"
-          
-        
-        
-      },
-      {
-        id: 3,
-        title: "Bidding System",
-        description: ["Developed user-friendly interfaces for browsing auction items and placing bids",
-"Integrated real-time bidding updates in bidder side and notifications to enhance user experience using local database",
-"Implemented secure authentication and authorization mechanisms for user accounts",
-"Utilized Swift programming language and Xcode IDE for efficient iOS app development",
-"Designed and implemented backend functionality to manage auction data and bidding processes"],
-            technology: "Swift programming language and Xcode IDE were used for iOS app development. The app featured a local database for real-time bidding updates and notifications. Secure authentication and authorization mechanisms were implemented for user accounts, with backend functionality managing auction data and bidding processes.",
-            imgvid: "bidapp.mov"
-
-        
-        
-      },
-      {
-        id: 4,
-        title: "Dynamic Web Application",
-        description: ["Developed a dynamic web application using Django framework ",
-"Facilitated creation of posts, user registration, and login functionalities",
-"Secure authentication for registered users"],
-            technology: "he web application was developed using the Django framework. It included secure user authentication, post creation, and user registration and login functionalities.",
-             imgvid: "djangoprojectvideo.mov"
-       
-      }
-];
-module.exports=popup_projects;
+        {
+            id: 1,
+            title: "Gurbani Application",
+            description: ["Historical Information: Provides insights into Sikhism's rich history, culture, and traditions",
+    "Martyrs' Profiles: Honors the sacrifices of Sikh martyrs with dedicated profiles and tributes",
+    "Japji Sahib Analysis: Offers a detailed study and interpretation of Japji Sahib, enhancing users' understanding of Sikh scripture",
+    "User-Friendly Interface: Designed with intuitive navigation and user interface for ease of use and accessibility."],
+            technology: "The Gurbani Application was built in Xcode using Swift, with UIKit for UI, Core Data for offline data, and AVFoundation for Gurbani audio playback. It supports responsive design via Auto Layout and uses JSON for data handling, with GitHub for version control.",
+            imgvid: "gurugranthsahib.jpg"
+          },
+          {
+            id: 2,
+            title: "Children's Game Website",
+            description: ["Created user-friendly registration and authentication system",
+    "Implemented real-time validation using AJAX for enhanced user experience",
+    "Designed and managed multi-level question-and-answer game structure using php, css and html",
+    "Integrated secure password management functionality",
+    "Developed comprehensive game results tracking and display features",
+    "Utilized MySQL database for efficient data management",
+    "Customized user interface for enhanced interactivity and engagement"],
+                technology: "PHP, CSS, and HTML were employed to build the game structure and interface, while AJAX provided real-time validation for an enhanced user experience. MySQL was used for efficient data management, and secure password management ensured user authentication.",
+                imgvid: "kidsgamevideo.mov"
+              
+            
+            
+          },
+          {
+            id: 3,
+            title: "Bidding System",
+            description: ["Developed user-friendly interfaces for browsing auction items and placing bids",
+    "Integrated real-time bidding updates in bidder side and notifications to enhance user experience using local database",
+    "Implemented secure authentication and authorization mechanisms for user accounts",
+    "Utilized Swift programming language and Xcode IDE for efficient iOS app development",
+    "Designed and implemented backend functionality to manage auction data and bidding processes"],
+                technology: "Swift programming language and Xcode IDE were used for iOS app development. The app featured a local database for real-time bidding updates and notifications. Secure authentication and authorization mechanisms were implemented for user accounts, with backend functionality managing auction data and bidding processes.",
+                imgvid: "bidapp.mov"
+    
+            
+            
+          },
+          {
+            id: 4,
+            title: "Dynamic Web Application",
+            description: ["Developed a dynamic web application using Django framework ",
+    "Facilitated creation of posts, user registration, and login functionalities",
+    "Secure authentication for registered users"],
+                technology: "he web application was developed using the Django framework. It included secure user authentication, post creation, and user registration and login functionalities.",
+                 imgvid: "djangoprojectvideo.mov"
+           
+          },
+          {
+            id: 5,
+           title: "3D Character and Animation project",
+          description: ["Created a virtual simulation demo for nursing students who aspire to become doctors","Developed in Unity, with Blender and MakeHuman for 3D modeling","Allows students to play a game or explore different hospital units as a simulation before working in a real hospital","Provides an interactive learning environment with different hospital scenarios"],
+          technology: "Unity, Blender,Character Creator 4, MakeHuman,Unreal Engine",
+         imgvid: "animationOne.mp4",
+         imgvid2: "animationTwo.mp4",
+          }
+    ];
+    module.exports=popup_projects;
